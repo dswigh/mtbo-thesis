88bc397df965a7dbceb24583978482e6bcf84cc4
Daniel Wigh <dswigh@gmail.com>
2023-10-10 23:24:46 +0100

    add everything

---
+++ multitask/__init__.py
@@ -0,0 +1,7 @@
+from .utils import *
+from .suzuki_benchmark_training import *
+from .mt import NewMTBO, NewSTBO
+from .suzuki_emulator import SuzukiEmulator
+from .suzuki_data_utils import *
+from .mixed_gp_regression import MixedMultiTaskGP, LCMMultitaskGP
+from .kinetic_models import MITKinetics
---
+++ multitask/utils.py
@@ -0,0 +1,96 @@
+import ord_schema
+from summit import *
+from ord_schema import message_helpers, validations
+from ord_schema.proto import dataset_pb2
+from ord_schema.proto.reaction_pb2 import *
+from ord_schema.message_helpers import find_submessages
+from ord_schema import units
+
+from rdkit import Chem
+from pint import UnitRegistry
+
+from pathlib import Path
+from typing import List
+import pandas as pd
+
+ureg = UnitRegistry()
+
+__all__ = [
+    "get_pint_amount",
+    "get_smiles",
+    "calculate_total_volume",
+    "get_rxn_yield",
+    "ureg",
+    "get_reactant_smiles",
+]
+
+
+def get_pint_amount(amount: Amount):
+    """Get an amount in terms of pint units"""
+    kind = amount.WhichOneof("kind")
+    units = getattr(amount, kind).units
+    value = getattr(amount, kind).value
+    pint_value = None
+    if kind == "moles":
+        units_str = Moles.MolesUnit.Name(units)
+    elif kind == "volume":
+        units_str = Volume.VolumeUnit.Name(units)
+    elif kind == "mass":
+        units_str = Mass.MassUnit.Name(units)
+    return value * ureg(units_str.lower())
+
+
+def get_smiles(compound: Compound, canonicalize=True):
+    for identifier in compound.identifiers:
+        if identifier.type == CompoundIdentifier.SMILES:
+            smiles = identifier.value
+            return Chem.CanonSmiles(smiles)
+
+
+def calculate_total_volume(rxn_inputs, include_workup=False):
+    # Calculate total reaction volume
+    total_volume = 0.0 * ureg.ml
+    for k in rxn_inputs:
+        for component in rxn_inputs[k].components:
+            amount = component.amount
+            include_workup = (
+                True
+                if include_workup
+                else component.reaction_role != ReactionRole.WORKUP
+            )
+            if amount.WhichOneof("kind") == "volume" and include_workup:
+                total_volume += get_pint_amount(amount)
+    return total_volume
+
+
+def get_rxn_yield(outcome):
+    yields = []
+    for product in outcome.products:
+        for measurement in product.measurements:
+            if measurement.type == ProductMeasurement.YIELD:
+                yields.append(measurement.percentage.value)
+    if len(yields) > 1:
+        raise ValueError(
+            "More than one product with a yield in reaction outcome. This is ambiguous."
+        )
+    elif len(yields) == 0:
+        raise ValueError("No reaction yield found in reaction outcome.")
+    return yields[0]
+
+
+def get_reactant_smiles(reaction: Reaction) -> List[str]:
+    inputs = reaction.inputs
+    reactants = []
+    for inp in inputs:
+        components = inputs[inp].components
+        for c in components:
+            if c is not None:
+                if c.reaction_role == ReactionRole.REACTANT:
+                    reactants.extend(
+                        [
+                            id_.value
+                            for id_ in c.identifiers
+                            if id_.type == CompoundIdentifier.SMILES
+                        ]
+                    )
+    return reactants
